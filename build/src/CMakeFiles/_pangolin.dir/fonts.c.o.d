e5f1bfb995125a44d4e4a80175586df5a7d47e11
rancheng <752050943@qq.com>
2022-02-26 13:27:21 +0800

    build for python in conda

--- build/src/CMakeFiles/_pangolin.dir/fonts.c.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/_pangolin.dir/fonts.c.o: \
- /mnt/Data/test/pangolin/build/src/fonts.c /usr/include/stdc-predef.h
--- build/src/CMakeFiles/_pangolin.dir/utils/timer.cpp.o.d
+++
@@ -1,2 +0,0 @@
-src/CMakeFiles/_pangolin.dir/utils/timer.cpp.o: \
- /mnt/Data/test/pangolin/src/utils/timer.cpp /usr/include/stdc-predef.h
